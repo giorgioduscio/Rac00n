05cd7b9737e7a298ba35c756c63080579065db01
Giorgio_D'uscio <giorgio.duscio@gmail.com>
2024-03-01 10:32:16 +0100

    racoonCat

--- create.js
+++ create.js
@@ -1,16 +1,18 @@
-document.querySelector("navTop").innerHTML=`
-<a href=""><img src="https://cdn.iconscout.com/icon/free/png-256/free-menu-176-433842.png"></a>
+document.querySelector("nav.top").innerHTML=`
+<row>
+    <a href=""><img src="https://cdn.iconscout.com/icon/free/png-256/free-menu-176-433842.png"></a>
 
-<searchBar>
-    <input type="text" placeholder="Ricerca">
-    <button></button>
-</searchBar>
-<a href=""><img src="https://cdn-icons-png.freepik.com/512/6337/6337309.png"></a>
+    <searchBar>
+        <input type="text" placeholder="Ricerca">
+        <button></button>
+    </searchBar>
+    <a href=""><img src="https://cdn-icons-png.freepik.com/512/6337/6337309.png"></a>
+</row>
 `;
 
 
 
-document.querySelector("navBottom").innerHTML=`
+document.querySelector("nav.bottom").innerHTML=`
     <row>
         <a href=""><img src="https://cdn.icon-icons.com/icons2/1660/PNG/512/3844470-home-house_110332.png"></a>
         <a href=""><img src="https://cdn-icons-png.freepik.com/512/6337/6337309.png"></a>
