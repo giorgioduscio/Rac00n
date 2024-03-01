e0d389f1a513a9e7cd83029decd30cf66bb0c3b6
giorgioduscio <119526157+giorgioduscio@users.noreply.github.com>
2024-03-01 11:32:56 +0100

    Update create.js

--- create.js
+++ create.js
@@ -15,7 +15,6 @@ document.querySelector("nav.top").innerHTML=`
 document.querySelector("nav.bottom").innerHTML=`
     <row>
         <a href=""><img src="https://cdn.icon-icons.com/icons2/1660/PNG/512/3844470-home-house_110332.png"></a>
-        <a href=""><img src="https://cdn-icons-png.freepik.com/512/6337/6337309.png"></a>
         <a href=""><img src="https://static.vecteezy.com/system/resources/previews/020/389/173/non_2x/bubble-speech-icon-png.png"></a>
         <a href=""><img src="https://cdn.icon-icons.com/icons2/2645/PNG/512/cart_icon_160296.png"></a>
     </row>
