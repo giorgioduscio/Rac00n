0a18089f99baeddb7f0529fcc8749f4e010600c8
Giorgio_D'uscio <giorgio.duscio@gmail.com>
2024-03-04 07:22:13 +0100

    Home e Profilo

--- pages/create.js
+++ pages/create.js
@@ -1,16 +1,14 @@
 document.querySelector("nav.bottom").innerHTML=`
     <row>
         <a href="/pages/home.html"><img id="home" src="https://cdn.icon-icons.com/icons2/1660/PNG/512/3844470-home-house_110332.png"></a>
-        <a href=""><img id="comments" src="https://static.vecteezy.com/system/resources/previews/020/389/173/non_2x/bubble-speech-icon-png.png"></a>
-        <a href=""><img id="cart" src="https://cdn.icon-icons.com/icons2/2645/PNG/512/cart_icon_160296.png"></a>
+        <a href="/pages/cart.html"><img id="cart" src="https://cdn.icon-icons.com/icons2/2645/PNG/512/cart_icon_160296.png"></a>
         <a href="/pages/user.html"><img id="user" src="https://cdn-icons-png.freepik.com/512/6337/6337309.png"></a>
     </row>
 `;
+        // <a href=""><img id="comments" src="https://static.vecteezy.com/system/resources/previews/020/389/173/non_2x/bubble-speech-icon-png.png"></a>
 
 document.querySelector("nav.top").innerHTML=`
 <row>
-    <a href=""><img id="menu" src="https://cdn-icons-png.flaticon.com/512/660/660376.png"></a>
-
     <searchBar>
         <input type="text" placeholder="Ricerca">
         <button></button>
