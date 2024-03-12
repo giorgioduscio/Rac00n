743f637d78373e04a418fa8dbdb45bffd19b9447
Giorgio_D'uscio <giorgio.duscio@gmail.com>
2024-03-12 19:00:18 +0100

    sommary dinamico

--- pages/home/summary.js
+++ pages/home/summary.js
@@ -1,13 +1,19 @@
 // TODO Summary
+let summaryArray=[];
 function summaryItem(name,urlImage){
         this.name=name;
         this.urlImage=urlImage;
+
+        this.addItem = function() { 
+            //aggiunge i valori passati nell'array
+            summaryArray.push(this);
+        }
+        this.addItem(); // And neither is this.
 }
 
-let summaryArray=[];
-summaryArray[0]= new summaryItem("Sushi","https://www.todis.it/wp-content/uploads/2023/05/sushi-fatto-in-casa.jpg")
-summaryArray[1]= new summaryItem("Pizza","https://ilfattoalimentare.it/wp-content/uploads/2022/12/Depositphotos_505971914_L-1.jpg")
-console.log(summaryArray);
+new summaryItem("Sushi","https://www.todis.it/wp-content/uploads/2023/05/sushi-fatto-in-casa.jpg")
+new summaryItem("Pizza","https://ilfattoalimentare.it/wp-content/uploads/2022/12/Depositphotos_505971914_L-1.jpg")
+console.log("SummaryArray:",summaryArray);
 
 
 const writeSummary=document.querySelector(".writeSummary");
