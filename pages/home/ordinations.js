8dfb67a782ef4a3f1b614d8770dd71e3c8335b4a
Giorgio_D'uscio <giorgio.duscio@gmail.com>
2024-03-13 09:31:22 +0100

    ordinations dinamico

--- pages/home/ordinations.js
+++ pages/home/ordinations.js
@@ -1,22 +1,60 @@
+// TODO array
+const arrayOrdinations=[];
+function ordinationItems(name, price, ingredients, urlImage) {
+    this.name=name;
+    this.price=price;
+    this.ingredients=ingredients;
+    this.urlImage=urlImage;
+
+    this.addItems=function(){ arrayOrdinations.push(this); } 
+    this.addItems();
+}
+new ordinationItems("Margherita",4,"Pomodoro, mozzarella, basilico","https://ilfattoalimentare.it/wp-content/uploads/2022/12/Depositphotos_505971914_L-1.jpg")
+new ordinationItems("Romana",6,"Pomodoro, mozzarella, prociuto cotto","https://ilfattoalimentare.it/wp-content/uploads/2022/12/Depositphotos_505971914_L-1.jpg")
+console.log("arrayOrdinations:",arrayOrdinations);
+
 // TODO innerHTML
-const amount=document.querySelectorAll(".amount");
-for (let index = 0; index < amount.length; index++) {
-    amount[index].innerHTML=`
-        <h4>Quantità</h4>
-        <h3 class="output">0</h3>
-        <button class="increment">+</button>
-        <button class="decrement">-</button>
-        <button class="send">
-            <img src="https://cdn.icon-icons.com/icons2/2645/PNG/512/cart_icon_160296.png" alt="">   
-        </button>
-    `;
+// optimize righe
+const writeOrdinations=document.querySelector(".writeOrdinations");//seleziona dove scrivere
+for (let a = 0; a < arrayOrdinations.length; a++) {//per tante volte quanto i records dell'array
+    writeOrdinations.innerHTML+=`<row class="card"></row>`;//aggiungi una card
+}
+
+// optimize card
+const card=document.querySelectorAll(".card");//seleziona tutte le card
+for (let a = 0; a < card.length; a++) {//per tante volte quanto le card
+    card[a].innerHTML=`
+        <column class="show">
+            <div>
+                <img src="${arrayOrdinations[a].urlImage}">
+                <div>
+                    <h2 class="priceFood">${arrayOrdinations[a].price}€</h2>
+                    <div>
+                        <h2 class="nameFood">${arrayOrdinations[a].name}</h2>
+                        <h4 class="ingredientsFood">${arrayOrdinations[a].ingredients}</h4>
+                    </div>
+                </div>
+            </div>
+        </column>
+        <column class="amount">
+            <h5>Quantità</h5>
+            <h3 class="output">0</h3>
+            <button class="increment">+</button>
+            <button class="decrement">-</button>
+            <button class="send">
+                <img src="https://cdn.icon-icons.com/icons2/2645/PNG/512/cart_icon_160296.png" alt="">   
+            </button>
+        </column>
+    `;//aggiungi il testo
 }
 
 
+
 // TODO amount
-const output=document.querySelectorAll('.output');
-const increment=document.querySelectorAll('.increment');
-const decrement=document.querySelectorAll('.decrement');
+const amount=document.querySelectorAll('.amount');
+    const output=document.querySelectorAll('.output');
+    const increment=document.querySelectorAll('.increment');
+    const decrement=document.querySelectorAll('.decrement');
 const counter=[];
 for (let index = 0; index < amount.length; index++) {
     counter.push(0); // inizializza tutti i contatori
