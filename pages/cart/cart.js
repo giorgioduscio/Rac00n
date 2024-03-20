aca6d693ef3b52b0301ef521634a2cbaca264c7f
Giorgio_D'uscio <giorgio.duscio@gmail.com>
2024-03-20 22:25:06 +0100

    tavoli disponibili

--- pages/cart/cart.js
+++ pages/cart/cart.js
@@ -1,22 +1,40 @@
-// TODO carrello attuale
-const writeCart=document.querySelector('.writeCart');
-for (let a = 0; a < arrayProducts.length; a++) {
-    writeCart.innerHTML+=`<div class="product"></div>`;        
+// TODO tavoli
+const arrayTables=[];
+function Table(id,state) {
+    this.id=id;
+    this.state=state;
+
+    this.pushTable=function (){
+        arrayTables.push(this)
+    }
+    this.pushTable();
+}
+    new Table ('A123',false)
+    new Table ('G198',true)
+    new Table ('K491',true)
+    new Table ('R778',false)
+    new Table ('N973',true)
+    new Table ('Z456',true)
+    new Table ('C933',true)
+    new Table ('M951',false)
+
+// TODO mostra tavoli disponibili
+const generateTables=document.querySelector('.generateTables')
+for (let a = 0; a < arrayTables.length; a++) { //per ogni tavolo
+    let disponibility=arrayTables[a].state;
+    if (disponibility) {//se lo stato è disponibile
+        generateTables.innerHTML+=`<div class="tableItem">${arrayTables[a].id}</div>`;
+    }// optimize mostra il tavolo disponibile
 }
 
-const product=document.querySelectorAll('.product');
-console.log("product",product);
-for (let a = 0; a < product.length; a++) {
-    product[a].innerHTML=`
-        <div>
-            <row>
-                <h3>${arrayProducts[a].name}</h3> 
-                <h3 class="price">${arrayProducts[a].price}€</h3> 
-            </row>
-            <p>${arrayProducts[a].ingredients}</p> 
-        </div>
-`;    
+// TODO seleziona tavolo
+const tableItem=document.querySelectorAll('.tableItem');
+for (let a = 0; a < tableItem.length; a++) {//per ogni tavolo disponibile
+    tableItem[a].addEventListener('click',function(){//quando uno viene cliccato
+        console.log(tableItem[a]);
+        /*
+        nasconde tutti i tavoli
+        evidenzia il tavolo selezionato
+        */
+    })
 }
-
-// TODO cronologia
-
