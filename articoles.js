9b72edec8e3eb9ac16fbf812f4a1e18a16678f3c
Giorgio_D'uscio <giorgio.duscio@gmail.com>
2024-03-21 16:38:42 +0100

    arrayProdotti

--- articoles.js
+++ articoles.js
@@ -1,6 +1,7 @@
-// TODO array
+// TODO arrayProducts
 const arrayProducts=[];
-function arrayProductsItem(name, price, ingredients, imageUrl) {
+function arrayProductsObject(category, name, price, ingredients, imageUrl) {
+    this.category=category;
     this.name=name;
     this.price=price;
     this.ingredients=ingredients;
@@ -9,33 +10,80 @@ function arrayProductsItem(name, price, ingredients, imageUrl) {
     this.addItems=function(){ arrayProducts.push(this); } 
     this.addItems();
 }
-new arrayProductsItem(
-    "Margherita",
-    4,
-    "Pomodoro, mozzarella, basilico",
-    "https://ilfattoalimentare.it/wp-content/uploads/2022/12/Depositphotos_505971914_L-1.jpg"
-)
-new arrayProductsItem(
-    "Romana",
-    6,
-    "Pomodoro, mozzarella, prociuto cotto",
-    "https://www.pizzaontheroad.eu/wp-content/uploads/2019/09/ADL_4586.jpg"
-)
-new arrayProductsItem(
-    "San daniele",
-    6.60,
-    "Pomodoro, mozzarella, prociutto crudo, scaglie di grana padano",
-    "https://www.anticogatoleto.com/shop/wp-content/uploads/2021/02/sandaniele-min-scaled.jpeg"
-)
-new arrayProductsItem(
-    "Diavola",
-    6.70,
-    "Pomodoro, mozzarella, salame piccante",
-    "https://www.pizzarecipe.org/wp-content/uploads/2019/01/Pizza-Diavola.jpg"
-)
-new arrayProductsItem(
-    "Norvegese",
-    8.80,
-    "Mozzarella, salmone affumicato, pistacchio, formaggio spalmabile",
-    "https://3punto0restaurant.com/wp-content/uploads/2020/06/WhatsApp-Image-2023-03-17-at-20.01.53.jpeg"
-)
+    // optimize antipasti
+    new arrayProductsObject(
+        "Antipasti", "Patatine", 2, "Porzione di patatine fritte con salse katchup, majonaise e barbeque",
+        "https://www.motopizza2.com/wp-content/uploads/2021/07/90681-1.jpg"
+    )
+    new arrayProductsObject(
+        "Antipasti", "Antipasto misto", 2, "Porzioni di dalame, guanciale, prociutto cotto, prociutto crudo, grana padano, olive verdi",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZRQWatOmj8myeEurSoRtkugV1GEw1Hisp6g&usqp=CAU"
+    )
+
+    // optimize primi
+    new arrayProductsObject(
+        "Primi", "Farfallette al salmone", 8, "Pasta, salmone affumicato, panna, prezzemolo",
+        "https://blog.giallozafferano.it/ilchiccodimais/wp-content/uploads/2019/06/farfalle-al-salmone-affumicato-con-panna-ricetta-facile-e-veloce-il-chicco-di-mais.jpg"
+    )
+    new arrayProductsObject(
+        "Primi", "Spaghetti al ragù", 7.5, "Pasta, pomodoro, ragù di maiale, carote",
+        "https://www.cuocicuoci.com/wp-content/uploads/2022/05/rigatoni-al-ragu.jpg"
+    )
+
+    // optimize pizze
+    const margherita=new arrayProductsObject(
+        "Pizze", "Margherita", 4, "Pomodoro, mozzarella, basilico",
+        "https://ilfattoalimentare.it/wp-content/uploads/2022/12/Depositphotos_505971914_L-1.jpg"
+    )
+    const romana=new arrayProductsObject(
+        "Pizze", "Romana", 6, "Pomodoro, mozzarella, prociuto cotto",
+        "https://www.pizzaontheroad.eu/wp-content/uploads/2019/09/ADL_4586.jpg"
+    )
+
+    // optimize secondi di carne
+    new arrayProductsObject(
+        "Secondi carne", "Costolette di maiale", 8, "Carne di maiale con salsa teriaki",
+        "https://www.fattoincasadabenedetta.it/wp-content/uploads/2022/04/AdobeStock_491302793-1200x900.jpg"
+    )
+    new arrayProductsObject(
+        "Secondi carne", "Salsiccia di pollo", 9, "Salsiccia di pollo",
+        "https://www.donnamoderna.com/content/uploads/2023/05/fac82ee49783838d.jpg"
+    )
+    // optimize secondi di pesce
+    new arrayProductsObject(
+        "Secondi pesce", "Aragosta", 8, "Aragosta al forno con limone e olio",
+        "https://media-assets.lacucinaitaliana.it/photos/61fac87791ff55922ebd8f79/1:1/w_2560%2Cc_limit/empty"
+    )
+    new arrayProductsObject(
+        "Secondi pesce", "Pesce misto", 9, "Pesce spada, gamberi e seppie al forno",
+        "https://www.alberto-arienti.com/wp-content/uploads/2021/11/Antipasto-di-pesce-1300x975.jpg"
+    )
+    // optimize bibite
+    new arrayProductsObject(
+        "Bibite", "Acqua naturale", 8, "Acqua naturale",
+        "https://www.ristorantiweb.com/wp-content/uploads/sites/9/2015/07/SB-Prestige-Rose-Edition.jpg"
+    )
+    new arrayProductsObject(
+        "Bibite", "Acqua gassata", 9, "Acqua gassata",
+        "https://www.ristorantiweb.com/wp-content/uploads/sites/9/2015/07/SB-Prestige-Rose-Edition.jpg"
+    )
+
+
+// TODO arrayCategories
+const HHHarrayCategories=['Antipasti','Primi','Pizze','Secondi carne','Secondi pesce','Bibite'];
+const HHHarrayProducts=[
+    ['Patatine','Antipasto misto'],
+    ['Farfallette al salmone','Spaghetti al ragù'],
+    [margherita,romana],
+    ['Costolette di maiale','Salsiccia di pollo'],
+    ['Aragosta','Pesce misto'],
+    ['Acqua gassata','Acqua naturale'],
+]
+
+for (let a = 0; a < HHHarrayCategories.length; a++) {
+    console.log(HHHarrayCategories[a]);
+
+    for (let b = 0; b < HHHarrayProducts[a].length; b++) {
+        console.log(HHHarrayProducts[a][b]);
+    }
+}
